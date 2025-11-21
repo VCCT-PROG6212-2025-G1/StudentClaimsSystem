708eaabca098467ec54e704c7ac26f4e07f29f5d
ash-kruger <ST10440651@vcconnect.edu.za>
2025-11-21 15:42:33 +0200

    Created Models, Helpers, Services, ModelViews and Views as well as added their respective code

--- StudentClaimsSystem/MainWindow.xaml.cs
+++ StudentClaimsSystem/MainWindow.xaml.cs
@@ -14,14 +14,12 @@ using System.Windows.Shapes;
 
 namespace StudentClaimsSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
